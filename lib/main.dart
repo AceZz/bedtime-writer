a22bdc35bf6a383cad2c017d8e5294d658d27f8d
Pierre <pierre.wan-fat@mines-paristech.fr>
2023-02-11 16:00:17 +0100

    Set up Firebase
    Plugins: firebase_storage and cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'router.dart';
 import 'theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: MyApp()));
 }
 
