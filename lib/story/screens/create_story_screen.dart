4c457b7e1f64caa468e787cd4e150c0985dc2070
Pierre <pierre.wan-fat@mines-paristech.fr>
2023-02-12 15:58:59 +0100

    Refactor create_story_screen.dart
    All widgets used in CreateStoryScreen are gathered in create_story_screen.dart.
    This simplifies the lib/story/screens folder organisation.

--- lib/story/screens/create_story_screen.dart
+++ lib/story/screens/create_story_screen.dart
@@ -5,19 +5,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
 import '../../widgets/app_scaffold.dart';
+import '../../widgets/lottie_loading.dart';
 import '../backend/api_calls.dart';
 import '../backend/firebase.dart';
 import '../states/create_story_state.dart';
-import 'loading_content.dart';
-import 'question_content.dart';
+import '../states/story_params.dart';
 import 'story_image.dart';
 import 'story_widget.dart';
 
 /// Entry point of the story creation.
 ///
-/// Contains the logic that routes between the content (asking questions,
-/// loading the story and displaying the story). By "content", we mean the part
-/// of the screen which is inserted into the scaffold template.
+/// Depending on the current `CreateStoryState`, displays one among these:
+/// * the current question
+/// * a loading message
+/// * the generated story
 class CreateStoryScreen extends ConsumerWidget {
   const CreateStoryScreen({Key? key}) : super(key: key);
 
@@ -26,6 +27,7 @@ class CreateStoryScreen extends ConsumerWidget {
     var story = state.story;
     var storyImage = state.storyImage;
 
+    // Displays the story.
     if (story != null && storyImage != null) {
       var payload = _SavePayload(
         title: state.storyParams.title,
@@ -43,6 +45,7 @@ class CreateStoryScreen extends ConsumerWidget {
       );
     }
 
+    // Displays a loading screen.
     if (!state.hasQuestions) {
       // On page load action.
       SchedulerBinding.instance.addPostFrameCallback((_) async {
@@ -59,7 +62,6 @@ class CreateStoryScreen extends ConsumerWidget {
           storyText = apiResults[0];
           storyImage = apiResults[1];
         } catch (e) {
-          print(e.toString());
           storyText = 'Sorry, your story could not be generated';
           storyImage = '';
         }
@@ -69,10 +71,11 @@ class CreateStoryScreen extends ConsumerWidget {
             .setStory(storyText, storyImage);
       });
 
-      return LoadingContent();
+      return _LoadingContent();
     }
 
-    return QuestionContent(question: state.currentQuestion);
+    // Displays the current question.
+    return _QuestionContent(question: state.currentQuestion);
   }
 
   @override
@@ -130,6 +133,117 @@ class _SaveButton extends StatelessWidget {
       builder: (context) => ElevatedButton(
         onPressed: () => _onSave(context),
         child: const Text('Save this story'),
+      ),
+    );
+  }
+}
+
+/// Displays a loading screen.
+class _LoadingContent extends ConsumerWidget {
+  const _LoadingContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        LottieLoading(),
+        Text(
+          'Your story is being created...',
+          style: Theme.of(context).primaryTextTheme.headlineSmall,
+        ),
+      ],
+    );
+  }
+}
+
+/// Displays a question.
+class _QuestionContent extends StatelessWidget {
+  /// The maximum number of choices that can be displayed.
+  static const maxNumChoices = 3;
+
+  final Question question;
+
+  const _QuestionContent({
+    Key? key,
+    required this.question,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget questionText = Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        question.text,
+        textAlign: TextAlign.center,
+        style: Theme.of(context).primaryTextTheme.headlineMedium,
+      ),
+    );
+
+    List<Widget> choiceButtons = question.choices
+        .take(maxNumChoices)
+        .map((choice) => _ChoiceButton(choice: choice))
+        .toList();
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [questionText] + choiceButtons,
+    );
+  }
+}
+
+/// Displays a [Choice]: [Choice.icon] on top of [Choice.text].
+///
+/// When clicking on this widget, the state is updated using the provided
+/// [choice].
+class _ChoiceButton extends ConsumerWidget {
+  final Choice choice;
+
+  const _ChoiceButton({Key? key, required this.choice}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: make it more responsive.
+    var buttonWidth = 0.60 * MediaQuery.of(context).size.width;
+    var buttonHeight = 0.15 * MediaQuery.of(context).size.height;
+
+    var icon = Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Icon(
+        choice.icon,
+        size: 50,
+      ),
+    );
+    var text = Text(
+      choice.text,
+      textAlign: TextAlign.center,
+      style: Theme.of(context).primaryTextTheme.titleMedium,
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: InkWell(
+        onTap: () {
+          ref.read(createStoryStateProvider.notifier).updateStoryParams(choice);
+        },
+        borderRadius: BorderRadius.circular(20),
+        child: Ink(
+          width: buttonWidth,
+          height: buttonHeight,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [icon, text],
+          ),
+        ),
       ),
     );
   }
