97a938702e96dca325a82e192ebd232f6e32306d
AceZz <tristan.stampfler@gmail.com>
2023-02-20 21:33:50 +0100

    Add bottom padding to bottom story row widget

--- lib/story/screens/story_widget.dart
+++ lib/story/screens/story_widget.dart
@@ -114,7 +114,7 @@ class StoryWidget extends StatelessWidget {
     ];
 
     Widget iconsRow = Padding(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.only(top: 10, bottom: 50,),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: bottomWidgets,
