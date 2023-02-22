e5795e1907d88147157dd95a284f1c3b8cd431ab
Pierre <p+github@wanfat.fr>
2023-02-22 23:26:34 +0100

    backend: set up Firebase
    Set up Cloud Functions boilerplate.
    
    Set up Firestore and Cloud Storage emulators.

---
+++ functions/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "object-curly-spacing": ["error", "always"],
+  },
+};
