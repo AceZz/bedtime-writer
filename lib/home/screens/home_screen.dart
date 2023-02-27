15ea5c6c84a33d04bae28aa641a52583d2e0589b
AceZz <40855276+AceZz@users.noreply.github.com>
2023-02-27 17:39:39 +0100

    Beautify home screen
    * Create custom home screen button

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -2,9 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../story/index.dart';
 import '../../widgets/app_scaffold.dart';
 import '../../widgets/fade_in.dart';
+import '../../story/index.dart';
+
+class HomeScreenButton extends ConsumerWidget {
+  final String text;
+  final String destination;
+  final bool resetStoryState;
+
+  const HomeScreenButton({
+    Key? key,
+    required this.text,
+    required this.destination,
+    this.resetStoryState = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    Widget buttonTextWidget = Text(
+      text,
+      style: Theme.of(context).primaryTextTheme.headlineSmall,
+    );
+
+    return Container(
+      width: 0.7 * MediaQuery.of(context).size.width,
+      height: 60,
+      child: Material(
+        color: Theme.of(context).colorScheme.primary,
+        elevation: 10,
+        borderRadius: BorderRadius.circular(10),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: InkWell(
+          onTap: () {
+            if (resetStoryState) {
+              ref.read(createStoryStateProvider.notifier).reset();
+            }
+            context.pushNamed(destination);
+          },
+          child: Ink(
+            child: Center(child: buttonTextWidget),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,45 +67,17 @@ class HomeScreen extends ConsumerWidget {
       ),
     );
 
-    Widget newStoryButton = ElevatedButton(
-      onPressed: () {
-        ref.read(createStoryStateProvider.notifier).reset();
-        context.pushNamed('create_story');
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Text(
-          'New story',
-          style: Theme.of(context).primaryTextTheme.headlineSmall,
-        ),
-      ),
+    Widget newStoryButton = HomeScreenButton(
+      text: 'New story',
+      destination: 'create_story',
+      resetStoryState: true,
     );
 
-    Widget libraryButton = ElevatedButton(
-      onPressed: () {
-        context.pushNamed('library');
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Text(
-          'Library',
-          style: Theme.of(context).primaryTextTheme.headlineSmall,
-        ),
-      ),
-    );
+    Widget libraryButton =
+        HomeScreenButton(text: 'Library', destination: 'library');
 
-    Widget settingsButton = ElevatedButton(
-      onPressed: () {
-        context.pushNamed('settings');
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Text(
-          'Settings',
-          style: Theme.of(context).primaryTextTheme.headlineSmall,
-        ),
-      ),
-    );
+    Widget settingsButton =
+        HomeScreenButton(text: 'Settings', destination: 'settings');
 
     Widget menuWidget = Padding(
       padding: const EdgeInsets.only(top: 50.0),
@@ -74,8 +89,8 @@ class HomeScreen extends ConsumerWidget {
               (i, button) => MapEntry(
                   i,
                   FadeIn(
-                    duration: const Duration(milliseconds: 1500),
-                    delay: Duration(milliseconds: 500 + 1000 * (i + 1)),
+                    duration: const Duration(milliseconds: 500),
+                    delay: Duration(milliseconds: 500 + 500 * (i + 1)),
                     child: Padding(
                       padding: const EdgeInsets.only(bottom: 35.0),
                       child: button,
