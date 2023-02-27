ba5bf87b58c2d2aaed22c83ff4d8d48a66364b39
Pierre <p+github@wanfat.fr>
2023-02-27 23:43:20 +0100

    Add the frontend code to call addStory
    Add an option in the environment file to use the Firebase emulators.

--- README.md
+++ README.md
@@ -4,15 +4,37 @@
 
 All rights reserved to Tristan Stampfler and Pierre Wan-Fat
 
-## Code organization
+## Build
+
+Create a `.env` file in the same folder as the `pubspec.lock` file, with the following content:
+
+```
+USE_FIREBASE_EMULATORS=false
+```
+
+## Development
+
+### Code organization
 
 * `lib/`: contains the Flutter code
   * `feature_name/`: everything related to a specific feature
-    * `backend/`: calls to external APIs
     * `screens/`: screens and related widgets
     * `states/`: states and logic attached to them
     * `index.start`: should reexport the screens
   * `widgets/`: contains widgets that are reused across screens
+  * `backend.dart`: calls to the Firebase backend
   * `main.dart`: entry point of the application
   * `router.dart`: the routes of the application
   * `theme.dart`: the theme for the entire application
+
+### Backend emulation
+
+Before deploying your functions to Google Cloud, test them locally with the emulators. To use the
+Firebase emulators, give the `.env` file the following content:
+
+```
+USE_FIREBASE_EMULATORS=true
+```
+
+Then, go to the `functions` folder and follow the instructions of `functions/README.md`. When
+everything is set up, you can launch the emulators by running `npm run emulators`.
--- lib/story/backend/firebase.dart
+++ lib/story/backend/firebase.dart
@@ -1,7 +1,10 @@
+import 'package:bedtime_writer/story/states/story_params.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 final firestore = FirebaseFirestore.instance;
+final functions = FirebaseFunctions.instance;
 final storageRef = FirebaseStorage.instance.ref();
 
 /// A [CollectionReference] to the stories.
@@ -15,3 +18,24 @@ DocumentReference<Map<String, dynamic>> storyReference(String id) =>
 /// Returns a [Reference] to the image file for the story [id].
 Reference storyImageReference(String id) =>
     storageRef.child('stories/image/$id.jpg');
+
+Future<String> addStory(StoryParams params) async {
+  functions.useFunctionsEmulator('10.0.2.2', 5001);
+  print('called');
+
+  return functions.httpsCallable('addStory').call(
+    {
+      'character': {
+        'name': params.character?.name ?? '',
+        'type': params.character?.type ?? '',
+        'power': params.character?.power ?? '',
+        'challenge': params.character?.challenge ?? '',
+      },
+      'age': params.age ?? '',
+      'duration': params.duration ?? '',
+      'place': params.place ?? '',
+      'object': params.object ?? '',
+      'moral': params.moral ?? '',
+    },
+  ).then((result) => result.data);
+}
