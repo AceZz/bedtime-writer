710e7eb78c45775c0d54c6cb79ebb585e3314f52
Pierre <p+github@wanfat.fr>
2023-03-11 20:40:19 +0100

    frontend: add Firebase Auth
    Use the Firebase Auth emulator

--- lib/utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-bool useFirebaseEmulators() {
-  final config = dotenv.get('USE_FIREBASE_EMULATORS', fallback: 'false');
-  return config.toLowerCase() == 'true';
-}
