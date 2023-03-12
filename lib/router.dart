94ad9f24811d7d5fde28e0c64d4c21cac4b5e26a
Pierre <p+github@wanfat.fr>
2023-03-12 23:26:15 +0100

    frontend: Add sign-up screen
    Add a screen for signing up with Google, and the appropriate
    redirections in the router.

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'account/index.dart';
+import 'backend/index.dart';
 import 'home/index.dart';
 import 'settings/index.dart';
 import 'story/index.dart';
 
+final _key = GlobalKey<NavigatorState>();
+
+/// Returns the redirection to the sign-in page if the user is not registered.
+String? _unregisteredRedirect(Ref ref, {String? redirect}) {
+  final user = ref.read(userProvider);
+
+  if (user is RegisteredUser) {
+    return null;
+  }
+
+  return Uri(
+    path: '/account/sign-in',
+    queryParameters: {'redirect': redirect},
+  ).toString();
+}
+
 final routerProvider = Provider<GoRouter>((ref) {
+  // We do not want to rebuild the router every time `userProvider` changes.
+  // As a result, it is only `read` *inside* callbacks (such as `builder` or
+  // `_signInRedirect`).
+
   return GoRouter(
+    navigatorKey: _key,
     routes: [
       GoRoute(
         name: 'home',
@@ -14,20 +38,42 @@ final routerProvider = Provider<GoRouter>((ref) {
         builder: (context, state) => HomeScreen(),
       ),
       GoRoute(
+        name: 'sign_in',
+        path: '/account/sign-in',
+        builder: (context, state) {
+          final redirect = state.queryParams['redirect'] ?? 'home';
+          return SignInScreen(redirect: redirect);
+        },
+      ),
+      GoRoute(
         name: 'create_story',
         path: '/story/new',
-        builder: (context, state) => CreateStoryScreen(),
+        builder: (context, state) {
+          final user = ref.read(userProvider.select((user) => user));
+          if (user is UnauthUser) {
+            return FutureBuilder(
+              future: user.signInAnonymously(),
+              builder: (context, snapshot) => const CreateStoryScreen(),
+            );
+          }
+          return const CreateStoryScreen();
+        },
       ),
       GoRoute(
         name: 'library',
         path: '/story/library',
         builder: (context, state) => LibraryScreen(),
+        redirect: (context, state) =>
+            _unregisteredRedirect(ref, redirect: state.path),
       ),
       GoRoute(
         name: 'display_story',
         path: '/story/library/:id',
-        redirect: (context, state) =>
-            state.params['id'] == null ? '/story/library' : null,
+        redirect: (context, state) {
+          final redirect = _unregisteredRedirect(ref, redirect: state.path);
+          final library = state.params['id'] == null ? '/story/library' : null;
+          return redirect ?? library;
+        },
         builder: (context, state) =>
             DisplayStoryScreen(id: state.params['id'] ?? ''),
       ),
@@ -35,6 +81,8 @@ final routerProvider = Provider<GoRouter>((ref) {
         name: 'settings',
         path: '/settings',
         builder: (context, state) => SettingsScreen(),
+        redirect: (context, state) =>
+            _unregisteredRedirect(ref, redirect: state.path),
       ),
     ],
   );
