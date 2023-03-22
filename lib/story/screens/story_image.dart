9661f1038a8d06867987ea2709bcfe80cebdd14e
AceZz <tristan.stampfler@gmail.com>
2023-03-22 21:58:44 +0100

    Revert "Change image display to rectangular"
    This reverts commit 1af26e6a3d7e97bab166bd8c731146983cccc455.

--- lib/story/screens/story_image.dart
+++ lib/story/screens/story_image.dart
@@ -81,18 +81,15 @@ class StoryImageDecoration extends StatelessWidget {
       alignment: Alignment.center,
       children: [
         // Wraps the image in a circle
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                image: DecorationImage(
-                  image: image,
-                  fit: BoxFit.fill,
-                )),
-          ),
+        Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: image,
+                fit: BoxFit.fill,
+              )),
         ),
         _LinearFadeWidget(width: width, height: height),
         _EdgesFadeWidget(width: width, height: height),
@@ -127,7 +124,7 @@ class _LinearFadeWidget extends StatelessWidget {
               Theme.of(context).colorScheme.background
             ],
           ),
-          shape: BoxShape.rectangle,
+          shape: BoxShape.circle,
         ),
       ),
     );
@@ -146,52 +143,20 @@ class _EdgesFadeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(0),
-      child: Stack(
-        children: [
-          Container(
-            width: width,
-            height: height,
-            color: Colors.transparent,
-          ),
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0, 0.1, 0.9, 1],
-                  colors: [
-                    Theme.of(context).colorScheme.background,
-                    Colors.transparent,
-                    Colors.transparent,
-                    Theme.of(context).colorScheme.background,
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  stops: [0, 0.1, 0.9, 1],
-                  colors: [
-                    Theme.of(context).colorScheme.background,
-                    Colors.transparent,
-                    Colors.transparent,
-                    Theme.of(context).colorScheme.background,
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          stops: [0, 0.9, 1],
+          colors: [
+            Colors.transparent,
+            Colors.transparent,
+            Theme.of(context).colorScheme.background
+          ],
+        ),
+        shape: BoxShape.circle,
       ),
     );
   }
 }
-
