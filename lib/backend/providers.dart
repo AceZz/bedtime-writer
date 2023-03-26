52547cabe403d61b8c1fa539c75891a88ca7900a
Pierre <p+github@wanfat.fr>
2023-03-26 15:37:34 +0400

    frontend: implement Story for Firebase
    Create providers that:
    * stream the stories of the current user
    * stream a specific story

--- lib/backend/providers.dart
+++ lib/backend/providers.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'firebase/index.dart';
+import 'story.dart';
 import 'user.dart';
 
-/// The main provider for everything user-related.
-///
-/// Note: this is where the "concrete" implementation of [User] is chosen.
+// Note: this file can be seen as where the concrete implementation of all
+// abstract classes are chosen.
+
+/// Provides the current [User].
 final userProvider = Provider<User>((ref) => getFirebaseUser(ref));
+
+/// Streams the [Story]s authored by the current [User].
+final AutoDisposeStreamProvider<List<Story>> userStoriesProvider =
+    firebaseUserStoriesProvider;
+
+/// Streams a specific [Story].
+final AutoDisposeStreamProviderFamily<Story, String> storyProvider =
+    firebaseStoryProvider;
