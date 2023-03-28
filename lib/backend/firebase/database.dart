a10e3a8247d504e95f9f9f006803559f8b3ebb25
Pierre <p+github@wanfat.fr>
2023-03-28 23:25:08 +0400

    frontend: create a generic function addStory

--- lib/backend/firebase/database.dart
+++ lib/backend/firebase/database.dart
@@ -2,15 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-import '../story_params.dart';
-
 final firebaseFirestore = FirebaseFirestore.instance;
 final firebaseFunctions = FirebaseFunctions.instance;
 final firebaseStorage = FirebaseStorage.instance;
-
-Future<String> addStory(StoryParams params) async {
-  return firebaseFunctions
-      .httpsCallable('addStory')
-      .call(params.serialize())
-      .then((result) => result.data);
-}
--- lib/backend/firebase/index.dart
+++ lib/backend/firebase/index.dart
@@ -1,3 +1,4 @@
 export 'emulators.dart' show configureFirebaseEmulators;
-export 'story.dart' show firebaseStoryProvider, firebaseUserStoriesProvider;
+export 'story.dart'
+    show firebaseAddStory, firebaseStoryProvider, firebaseUserStoriesProvider;
 export 'user.dart' show getFirebaseUser;
