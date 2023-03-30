57ec33338088cd5911fa52db0ff7bb5dc866c05b
Pierre <p+github@wanfat.fr>
2023-03-30 22:06:30 +0400

    frontend: implement favorite button for story

--- lib/story/screens/favorite_button.dart
+++ lib/story/screens/favorite_button.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
-/// Marks the story as favorite (TODO).
+/// Displays a full heart if [isFavorite], an empty heart otherwise.
 class FavoriteButton extends StatelessWidget {
+  final bool isFavorite;
   final double iconSize;
+  final Function()? onPressed;
 
-  const FavoriteButton({Key? key, required this.iconSize}) : super(key: key);
+  const FavoriteButton({
+    Key? key,
+    required this.isFavorite,
+    required this.iconSize,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       iconSize: iconSize,
       icon: Icon(
-        Icons.favorite,
+        isFavorite ? Icons.favorite : Icons.favorite_border,
         color: Theme.of(context).textTheme.bodyMedium?.color,
       ),
-      onPressed: null,
+      onPressed: onPressed,
     );
   }
 }
