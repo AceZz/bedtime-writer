9e7029c64382c9881eca2fc061e7e2aebfc11265
Pierre <p+github@wanfat.fr>
2023-04-10 16:33:41 +0400

    backend: remove JS code
    This prepares the migration to TypeScript.

--- functions/auth.js
+++
@@ -1,7 +0,0 @@
-export const getUid = (context) => {
-  const uid = context?.auth?.uid ?? null;
-  if (uid !== null) {
-    return uid;
-  }
-  throw new Error("User is not authenticated.");
-};
--- functions/utils.js
+++
@@ -1,9 +0,0 @@
-import sharp from "sharp";
-
-/**
- * Compress an image to a PNG, following the provided parameters (which are passed to sharp.png).
- * @returns a Buffer.
- */
-export async function compressPNG(input, parameters) {
-  return await sharp(input).png(parameters).toBuffer();
-}
