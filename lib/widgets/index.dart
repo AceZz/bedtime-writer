cd8eaff230e834df3232e5601ddc053e91a5ae23
AceZz <tristan.stampfler@gmail.com>
2023-04-14 18:47:27 +0200

    Create SignOutButton widget

--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -2,3 +2,4 @@ export 'app_scaffold.dart' show AppScaffold;
 export 'fade_in.dart' show FadeIn;
 export 'share_button.dart' show ShareButton;
 export 'sign_in.dart' show GoogleSignInButton;
+export 'sign_out.dart' show SignOutButton;
---
+++ lib/widgets/sign_out.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignOutButton extends StatelessWidget {
+  final String text;
+  final void Function() onPressed;
+
+  const SignOutButton(
+      {Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return FilledButton(
+      onPressed: onPressed,
+      child: Text(text),
+      style: FilledButton.styleFrom(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
