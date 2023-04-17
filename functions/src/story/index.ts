c433fedec87ad2d00cd1b53d9ff681e30012253c
Pierre <pierre.wan-fat@mines-paristech.fr>
2023-04-17 17:45:48 +0400

    backend: remove StoryParams and utils.ts
    These files are replaced by StoryLogic.

--- functions/src/story/index.ts
+++ functions/src/story/index.ts
@@ -3,4 +3,3 @@ export * from "./logic";
 export * from "./writer";
 export { StoryMetadata } from "./story_metadata";
 export { StoryPart } from "./story_part";
-export { StoryParams } from "./story_params";
