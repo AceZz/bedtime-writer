ab65c2057b0bb4a29b269373531939ed4a7d4c93
AceZz <tristan.stampfler@gmail.com>
2023-04-17 08:41:27 +0200

    Add controller and hint text to textfield

--- lib/widgets/textField.dart
+++ lib/widgets/textField.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({Key? key}) : super(key: key);
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +21,26 @@ class MyTextField extends StatelessWidget {
       fontSize: 16,
     );
 
+    TextStyle hintStyle = GoogleFonts.outfit(
+      color: Colors.grey.shade400,
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
+    );
+
     return TextField(
+        controller: controller,
+        obscureText: obscureText,
         style: inputTextStyle,
         decoration: InputDecoration(
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade800)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade800)),
           focusedBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
           ),
           fillColor: Colors.grey.shade200,
           filled: true,
+          hintText: hintText,
+          hintStyle: hintStyle,
         ));
   }
 }
