9c95813ebdbb773adba0aec7b89d6c1a58758c10
AceZz <tristan.stampfler@gmail.com>
2023-04-17 07:55:00 +0200

    Create custom textfield widget

---
+++ lib/widgets/textField.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle inputTextStyle = GoogleFonts.outfit(
+      color: Colors.black54,
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
+    );
+
+    return TextField(
+        style: inputTextStyle,
+        decoration: InputDecoration(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade800),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+        ));
+  }
+}
