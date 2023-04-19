b7d4c37e93c00aa8f8c3136664786035a5e0d5ba
Pierre <p+github@wanfat.fr>
2023-04-19 14:15:40 +0400

    backend: add StoryRequestStatus

--- functions/src/story/request/story_request_firestore_converter.ts
+++ functions/src/story/request/story_request_firestore_converter.ts
@@ -1,4 +1,5 @@
 import { StoryRequest } from "./story_request";
+import { StoryRequestStatus } from "./story_request_status";
 
 /**
  * Converts between a Firestore object and a specific type of StoryRequest.
@@ -6,5 +7,10 @@ import { StoryRequest } from "./story_request";
 export interface StoryRequestFirestoreConverter<T extends StoryRequest> {
   get(id: string): Promise<T>;
 
+  /**
+   * Write the request data to Firestore.
+   */
   write(request: T): Promise<string>;
+
+  updateStatus(id: string, status: StoryRequestStatus): Promise<void>;
 }
