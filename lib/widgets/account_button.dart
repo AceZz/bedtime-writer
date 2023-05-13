7cd184e11775405cdabbf2184f72f66be5f0a704
AceZz <tristan.stampfler@gmail.com>
2023-05-13 16:24:52 +0200

    Make account button responsive

--- lib/widgets/account_button.dart
+++ lib/widgets/account_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 import '../backend/index.dart';
@@ -49,7 +50,7 @@ class _AccountButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      iconSize: 40,
+      iconSize: 40.sp,
       icon: Icon(
         Icons.account_circle,
         color: Theme.of(context).primaryTextTheme.bodyMedium?.color,
