c27404f8336ec39ff6428bc724e672839599610d
Pierre <pierre.wan-fat@mines-paristech.fr>
2023-06-13 00:25:33 +0200

    admin: add add_story_form script
    This script can be used to fill the story forms collection.

---
+++ admin/src/add_story_form.ts
@@ -0,0 +1,55 @@
+/**
+ * Add a story form corresponding to the provided YAML file (defaults to
+ * DEFAULT_YAML_PATH).
+ */
+
+import { prompt } from "./utils";
+import {
+  firebaseEmulatorsAreUsed,
+  getFirebaseProject,
+  initFirebase,
+} from "./firebase_utils";
+import { FirestoreFormWriter, YAMLFormReader } from "./story";
+
+const DEFAULT_COLLECTION_NAME = "story__forms";
+const DEFAULT_YAML_PATH = "data/story/form.yaml";
+
+main().then(() => process.exit(0));
+
+async function main() {
+  const collectionName = DEFAULT_COLLECTION_NAME;
+  const yamlPath = getYamlPath();
+
+  if (await confirm(collectionName, yamlPath)) {
+    initFirebase();
+
+    const reader = new YAMLFormReader(yamlPath);
+    const form = await reader.read();
+
+    const writer = new FirestoreFormWriter(collectionName);
+    await writer.write(form);
+    console.log(`Form saved to ${DEFAULT_COLLECTION_NAME}.`);
+  } else {
+    console.log("Abort");
+  }
+}
+
+function getYamlPath(): string {
+  return process.argv.at(2) ?? DEFAULT_YAML_PATH;
+}
+
+async function confirm(
+  collectionName: string,
+  yamlPath: string
+): Promise<boolean> {
+  const projectLog = firebaseEmulatorsAreUsed()
+    ? "of Firestore emulator"
+    : `of project ${getFirebaseProject()}`;
+
+  const answer = await prompt(
+    `The collection ${collectionName} ${projectLog} will be added the ` +
+      `content of ${yamlPath}. Proceed? (y/N) `
+  );
+
+  return ["yes", "y"].includes(answer?.toLowerCase() ?? "no");
+}
