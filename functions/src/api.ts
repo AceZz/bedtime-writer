89c60fd41fe9873e511f5e011ed392c6c6d33be9
AceZz <tristan.stampfler@gmail.com>
2023-07-27 20:24:04 +0200

    Refactor apis

---
+++ functions/src/api.ts
@@ -0,0 +1,33 @@
+import { logger } from "./logger";
+import { getOpenAiApi } from "./open_ai";
+import {
+  FakeImageApi,
+  FakeTextApi,
+  ImageApi,
+  OpenAiImageApi,
+  OpenAiTextApi,
+  TextApi,
+} from "./story";
+
+export function getTextApi(): TextApi {
+  if (process.env.TEXT_API?.toLowerCase() === "fake") {
+    logger.info("using FakeTextApi");
+    return new FakeTextApi();
+  }
+
+  logger.info("using OpenAiTextApi");
+  return new OpenAiTextApi(
+    getOpenAiApi(process.env.OPENAI_API_KEY),
+    "gpt-3.5-turbo"
+  );
+}
+
+export function getImageApi(): ImageApi {
+  if (process.env.IMAGE_API?.toLowerCase() === "fake") {
+    logger.info("using FakeImageApi");
+    return new FakeImageApi();
+  }
+
+  logger.info("using OpenAiImageApi");
+  return new OpenAiImageApi(getOpenAiApi(process.env.OPENAI_API_KEY));
+}
