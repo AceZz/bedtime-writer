0153780221cd0923bdd8261dd9e590a1be65910b
AceZz <tristan.stampfler@gmail.com>
2023-08-15 21:08:08 +0200

    remove unused object

--- functions/test/user/data/user_feedback.ts
+++ functions/test/user/data/user_feedback.ts
@@ -2,14 +2,8 @@ import { UserFeedback } from "../../../src/user/feedback";
 /**
  * A dummy feedback.
  */
-export const FEEDBACK_JSON_0 = {
-  text: "test-feedback",
-  datetime: new Date(),
-  uid: "test-user",
-};
-
 export const FEEDBACK_0 = new UserFeedback(
-  FEEDBACK_JSON_0.text,
-  FEEDBACK_JSON_0.datetime,
-  FEEDBACK_JSON_0.uid
+  "test-feedback",
+  new Date(),
+  "test-uid"
 );
