3b8ed939ceabe5d39660f13256f01f3982ed50bf
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-15 12:52:32 +0200

    backend: move story cache data to test/story/data

--- functions/test/story/data/story_question.ts
+++ functions/test/story/data/story_question.ts
@@ -73,3 +73,42 @@ export const QUESTIONS_1 = async () => [
     ),
   ]),
 ];
+
+export const QUESTIONS_CHARACTER = async () => [
+  new StoryQuestion(
+    "characterNameV1",
+    "characterName",
+    "What is the character name?",
+    0,
+    new Date(2023, 7, 15),
+    [
+      await StoryChoice.fromImagePath(
+        "name0",
+        "New name 0",
+        "This is name0.",
+        "test/story/data/choice.jpg"
+      ),
+      await StoryChoice.fromImagePath(
+        "name1",
+        "New name 1",
+        "This is name1.",
+        "test/story/data/choice.jpg"
+      ),
+    ]
+  ),
+  new StoryQuestion(
+    "characterFlawV1",
+    "characterFlaw",
+    "What is the character flaw?",
+    1,
+    new Date(2023, 7, 15),
+    [
+      await StoryChoice.fromImagePath(
+        "flaw0",
+        "New flaw 0",
+        "This is flaw0.",
+        "test/story/data/choice.jpg"
+      ),
+    ]
+  ),
+];
---
+++ functions/test/story/data/story_request.ts
@@ -0,0 +1,57 @@
+import {
+  CACHE_AUTHOR,
+  CLASSIC_LOGIC,
+  StoryRequestV1,
+} from "../../../src/story";
+import { FORM_CHARACTER_ID } from "./story_form";
+import { QUESTIONS_CHARACTER } from "./story_question";
+
+const DURATION = 1;
+const STYLE = "style1";
+
+/**
+ * Works with FORM_CHARACTER.
+ */
+async function REQUEST_CHARACTER_0(
+  author: string = CACHE_AUTHOR
+): Promise<StoryRequestV1> {
+  const [questionCharacterName, questionCharacterFlaw] =
+    await QUESTIONS_CHARACTER();
+
+  return new StoryRequestV1(CLASSIC_LOGIC, {
+    author: author,
+    formId: FORM_CHARACTER_ID,
+    duration: DURATION,
+    style: STYLE,
+    characterName: questionCharacterName.choices[0].id,
+    characterFlaw: questionCharacterFlaw.choices[0].id,
+  });
+}
+
+/**
+ * Works with FORM_CHARACTER.
+ */
+async function REQUEST_CHARACTER_1(
+  author: string = CACHE_AUTHOR
+): Promise<StoryRequestV1> {
+  const [questionCharacterName, questionCharacterFlaw] =
+    await QUESTIONS_CHARACTER();
+
+  return new StoryRequestV1(CLASSIC_LOGIC, {
+    author: author,
+    formId: FORM_CHARACTER_ID,
+    duration: DURATION,
+    style: STYLE,
+    characterName: questionCharacterName.choices[1].id,
+    characterFlaw: questionCharacterFlaw.choices[0].id,
+  });
+}
+
+/**
+ * Works with FORM_CHARACTER.
+ */
+export async function REQUESTS_CHARACTER(
+  author: string = CACHE_AUTHOR
+): Promise<StoryRequestV1[]> {
+  return [await REQUEST_CHARACTER_0(author), await REQUEST_CHARACTER_1(author)];
+}
