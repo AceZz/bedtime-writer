327fd5f813739ea7eb9abdb0c35a4ca264fa85a4
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-15 12:09:04 +0200

    backend: refactor FirestoreStoryTestUtils

--- functions/test/story/utils/firestore_story_test_utils.ts
+++ functions/test/story/utils/firestore_story_test_utils.ts
@@ -1,49 +1,32 @@
 import { DocumentReference, getFirestore } from "firebase-admin/firestore";
-import {
-  FirebaseStoryWriter,
-  StoryGenerator,
-  StoryStatus,
-} from "../../../src/story";
+import { StoryMetadata, StoryStatus } from "../../../src/story";
 import { FirestoreStories } from "../../../src/firebase";
 import { expect } from "@jest/globals";
-import { GENERATOR_0, METADATA_0, STORY_ID_0 } from "../data/";
 
 /**
  * Helper class to interact with the story questions Firestore collection.
  */
-export class FirestoreStoryTestUtils {
-  constructor(private readonly stories: FirestoreStories) {}
-
-  get writer(): FirebaseStoryWriter {
-    return new FirebaseStoryWriter(this.stories, METADATA_0, STORY_ID_0);
-  }
-
-  get generator(): StoryGenerator {
-    return GENERATOR_0;
-  }
-
-  async createDoc(): Promise<void> {
-    this.stories.storyRef(STORY_ID_0).create({});
-  }
-
-  async getStoryData(): Promise<FirebaseFirestore.DocumentData | undefined> {
-    return (await this.stories.storyRef(STORY_ID_0).get()).data();
-  }
-
-  async expectComplete(): Promise<void> {
-    const data = await this.getStoryData();
+export class FirestoreStoryTestUtils extends FirestoreStories {
+  async expectComplete(id: string): Promise<void> {
+    const data = await this.getStoryData(id);
     expect(data?.status).toBe(StoryStatus.COMPLETE);
   }
 
-  async expectParts(): Promise<void> {
-    const data = await this.getStoryData();
+  async expectParts(id: string): Promise<void> {
+    const data = await this.getStoryData(id);
     expect(data?.parts.length).toBeGreaterThanOrEqual(1);
   }
 
-  async expectMetadata(): Promise<void> {
-    const data = await this.getStoryData();
-    expect(data?.isFavorite).toBe(METADATA_0.isFavorite);
-    expect(data?.title).toBe(METADATA_0.title);
+  async expectMetadata(id: string, metadata: StoryMetadata): Promise<void> {
+    const data = await this.getStoryData(id);
+    expect(data?.isFavorite).toBe(metadata.isFavorite);
+    expect(data?.title).toBe(metadata.title);
+  }
+
+  private async getStoryData(
+    id: string
+  ): Promise<FirebaseFirestore.DocumentData | undefined> {
+    return (await this.storyRef(id).get()).data();
   }
 
   /**
@@ -51,11 +34,9 @@ export class FirestoreStoryTestUtils {
    *
    * Firebase must be initialized before calling this function.
    */
-  async deleteCollection(): Promise<void> {
+  async delete(): Promise<void> {
     const firestore = getFirestore();
-    const questions = await firestore
-      .collection(this.stories.collectionPath)
-      .get();
+    const questions = await firestore.collection(this.collectionPath).get();
     await Promise.all(
       questions.docs.map((story) => this.deleteStory(story.ref))
     );
