3083ce99d8e250c468cab79f12b368ae020573d3
AceZz <tristan.stampfler@gmail.com>
2023-08-15 22:05:15 +0200

    increase timeout

--- functions/test/setup.ts
+++ functions/test/setup.ts
@@ -1,4 +1,11 @@
 import { jest } from "@jest/globals";
+import { initEnv } from "../src/firebase";
+import { parseEnvAsNumber } from "../src/utils";
+
+initEnv();
+
+// Increase timeout locally
+jest.setTimeout(parseEnvAsNumber("JEST_TIMEOUT", 5000));
 
 // Silence the logger.
 jest.mock("../src/logger");
