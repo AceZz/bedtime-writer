347645080d80f66c53d702f6fcd2b2e24b43b5ff
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-15 19:46:52 +0200

    backend: add combinations
    `combinations` yields the k among items combinations.

--- functions/test/utils.test.ts
+++ functions/test/utils.test.ts
@@ -7,6 +7,7 @@ import {
   cartesianProduct,
   listToMapById,
   combinationsIndices,
+  combinations,
 } from "../src/utils";
 import { FAKE_IMAGE_BYTES } from "../src/story";
 
@@ -35,6 +36,55 @@ describe("cartesianProduct", () => {
   test("Throws on empty input", () => {
     const input = [[], ["a"]];
     expect(() => cartesianProduct(input)).toThrow();
+  });
+});
+
+describe("combinations", () => {
+  const [a, b, c, d] = [1, 2, 3, 4];
+
+  test("throws on k < 0", () => {
+    expect(() => Array.from(combinations(-1, [a, b, 3]))).toThrow();
+  });
+
+  test("throws on k > items length", () => {
+    expect(() => Array.from(combinations(2, []))).toThrow();
+    expect(() => Array.from(combinations(5, [a, b, c]))).toThrow();
+  });
+
+  test("k = 0", () => {
+    expect(Array.from(combinations(0, []))).toEqual([[]]);
+    expect(Array.from(combinations(0, [a, b, c]))).toEqual([[]]);
+  });
+
+  test("k = 1", () => {
+    expect(Array.from(combinations(1, [a, b, c, d]))).toEqual([
+      [a],
+      [b],
+      [c],
+      [d],
+    ]);
+  });
+
+  test("k = 2", () => {
+    expect(Array.from(combinations(2, [a, b, c, d]))).toEqual([
+      [a, b],
+      [a, c],
+      [a, d],
+      [b, c],
+      [b, d],
+      [c, d],
+    ]);
+  });
+
+  test("duplicates", () => {
+    expect(Array.from(combinations(2, [a, b, b, b]))).toEqual([
+      [a, b],
+      [a, b],
+      [a, b],
+      [b, b],
+      [b, b],
+      [b, b],
+    ]);
   });
 });
 
