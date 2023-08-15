3b84f0701d4fe091ea16ba226c02d1d5601902ee
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-15 12:02:53 +0200

    backend: move Story data to test/story/data
    This will make the tests easier to read and understand.

---
+++ functions/test/story/data/story_generator.ts
@@ -0,0 +1,17 @@
+import {
+  FakeImageApi,
+  FakeTextApi,
+  NPartStoryGenerator,
+} from "../../../src/story";
+import { CLASSIC_LOGIC_0 } from "./story_logic";
+
+/**
+ * A dummy generator.
+ */
+const TEXT_API = new FakeTextApi();
+const IMAGE_API = new FakeImageApi();
+export const GENERATOR_0 = new NPartStoryGenerator(
+  CLASSIC_LOGIC_0,
+  TEXT_API,
+  IMAGE_API
+);
---
+++ functions/test/story/data/story_metadata.ts
@@ -0,0 +1,7 @@
+import { StoryMetadata } from "../../../src/story";
+
+/**
+ * A dummy metadata.
+ */
+export const METADATA_0 = new StoryMetadata("author0", "title0");
+export const STORY_ID_0 = "story0";
