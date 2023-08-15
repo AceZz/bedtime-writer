c04d088058d6decb1b640599c1cb724910eed33a
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-15 12:11:43 +0200

    backend: move and rename FirestoreStoryTestUtils

--- functions/test/firebase/utils/index.ts
+++ functions/test/firebase/utils/index.ts
@@ -1,2 +1,3 @@
+export { FirestoreStoriesUtils } from "./firestore_stories_utils";
 export { FirestoreStoryFormsUtils } from "./firestore_story_forms_utils";
 export { FirestoreStoryQuestionsUtils } from "./firestore_story_questions_utils";
