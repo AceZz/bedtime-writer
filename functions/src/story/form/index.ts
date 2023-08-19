9ac9ed55649fa791d8c9707423fe991793299015
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-19 16:46:06 +0200

    backend: add questions_variations
    Helper to generate `StoryQuestion`s with combinations of choices taken
    from a base question.

--- functions/src/story/form/index.ts
+++ functions/src/story/form/index.ts
@@ -1,5 +1,6 @@
 export * from "./reader";
 export { StoryChoice } from "./story_choice";
+export { StoryFormManager } from "./manager/story_form_manager";
 export { StoryForm } from "./story_form";
 export { StoryQuestion } from "./story_question";
 export * from "./writer";
