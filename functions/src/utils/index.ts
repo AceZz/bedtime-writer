7dc77f8a68d7e12d782fefe471334c1b4bbad82c
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-19 20:21:25 +0200

    backend: add ithCartesianProduct

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -36,6 +36,36 @@ export function setsAreEqual<T>(set1: Set<T>, set2: Set<T>): boolean {
 }
 
 /**
+ * Return `cartesianProduct(arrays)[i]` without having to generate the whole
+ * product.
+ */
+export function ithCartesianProduct<T>(i: number, arrays: T[][]): T[] {
+  // We assume that `i` is `j * N + k`, where `j` is the index of the first
+  // items in the cartesian product of `arrays[:-1]`, `N` is
+  // `arrays[-1].length` and `k` is the index of the last item of the product
+  // in `arrays[-1]`.
+
+  if (i < 0) throw new Error(`ithCartesianProduct: i = ${i} < 0`);
+  if (i >= numCartesianProduct(arrays)) {
+    throw new Error(`ithCartesianProduct: index ${i} does not exist.`);
+  }
+
+  if (arrays.length === 0) return [];
+
+  const last = arrays.at(-1) ?? [];
+  if (last.length === 0) {
+    throw new Error("ithCartesianProduct: empty arrays are not allowed.");
+  }
+  const lastIndex = i % last.length;
+
+  const rest = arrays.slice(0, -1);
+  // Use Math.round just in case there are float issues with the division.
+  const restIndex = Math.round((i - lastIndex) / last.length);
+
+  return [...ithCartesianProduct(restIndex, rest), last[lastIndex]];
+}
+
+/**
  * Create the cartesian product of an array of <T> arrays.
  *
  * The generated terms follow a "lexographical" order (based on the order given
@@ -68,6 +98,20 @@ export function* cartesianProduct<T>(arrays: T[][]): Generator<T[]> {
       }
     }
   }
+}
+
+/**
+ * Return the number of items in the cartesian product.
+ */
+export function numCartesianProduct<T>(arrays: T[][]): number {
+  let num = 1;
+  for (const array of arrays) {
+    if (array.length === 0) {
+      throw new Error("numCartesianProduct: empty arrays are not allowed.");
+    }
+    num *= array.length;
+  }
+  return num;
 }
 
 /**
