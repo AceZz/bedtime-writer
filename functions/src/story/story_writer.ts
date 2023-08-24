49fd5543090bf93229e882fdc2b6bad79bb0d8e0
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-24 23:36:21 +0200

    backend: refactor StoryWriter

--- functions/src/story/story_writer.ts
+++ functions/src/story/story_writer.ts
@@ -1,35 +1,99 @@
 import { StoryGenerator } from "./generator";
+import { StoryLogic } from "./logic";
+import { StoryMetadata } from "./story_metadata";
 import { StoryPart } from "./story_part";
 
 /**
- * Interface to write stories to some output.
+ * Write stories to some output.
+ *
+ * The id of the location can either be provided through the constructor or
+ * chosen with a call to `writeInit()`.
  */
-export interface StoryWriter {
-  /**
-   * Write end-to-end a story from a generator and return its id.
-   */
-  writeFromGenerator(generator: StoryGenerator): Promise<string>;
+export abstract class StoryWriter {
+  constructor(protected id: string | undefined) {}
 
   /**
-   * Write the metadata of a new story and return its id.
+   * Write metadata to a new location and return its id.
+   *
+   * Throw if `this.id` is already defined.
    */
-  writeMetadata(): Promise<string>;
+  async writeInit(metadata: StoryMetadata): Promise<string> {
+    if (this.id !== undefined)
+      throw new Error("StoryWriter: this writer is already initialized.");
+
+    this.id = await this.writeInitMetadata(metadata);
+    return this.id;
+  }
+
+  /**
+   * Write metadata to a new location and return its id.
+   */
+  protected abstract writeInitMetadata(
+    metadata: StoryMetadata
+  ): Promise<string>;
+
+  /**
+   * Write a full story from a generator.
+   *
+   * Note: any existing part will be deleted first.
+   */
+  async writeFromGenerator(
+    logic: StoryLogic,
+    generator: StoryGenerator
+  ): Promise<void> {
+    if (this.id === undefined)
+      throw new Error(
+        "StoryWriter: this writer is not initialized, please call " +
+          "`this.writeInit` or provide an id to the constructor."
+      );
+
+    try {
+      await this.writeTitle(generator.title());
+      await this.writeLogic(logic);
+
+      await this.deleteParts();
+
+      for await (const part of generator.storyParts()) {
+        await this.writePart(part);
+      }
+      await this.writeStatusComplete();
+    } catch (error) {
+      await this.writeStatusError(error);
+    }
+  }
+
+  /**
+   * Write the title of the story.
+   */
+  protected abstract writeTitle(title: string): Promise<void>;
+
+  /**
+   * Write the logic of the story.
+   */
+  protected abstract writeLogic(logic: StoryLogic): Promise<void>;
+
+  /**
+   * Delete any existing parts / prompts / images.
+   */
+  protected abstract deleteParts(): Promise<void>;
 
   /**
    * Write a part of the story and return its id.
    */
-  writePart(part: StoryPart): Promise<string>;
+  protected abstract writePart(part: StoryPart): Promise<string>;
 
   /**
    * Mark the story as complete.
    *
-   * Note: calling `writePart` after `writeComplete` is undefined behaviour
-   * and should be avoided.
+   * Note: calling `writePart` after `writeStatusComplete` is undefined
+   * behaviour.
    */
-  writeComplete(): Promise<void>;
+  protected abstract writeStatusComplete(): Promise<void>;
 
   /**
    * Mark the story as having encountered an error.
+   *
+   * Note: calling `writePart` after `writeStatusError` is undefined behaviour.
    */
-  writeError(): Promise<void>;
+  protected abstract writeStatusError(error: unknown): Promise<void>;
 }
--- functions/test/story/data/story_logic.ts
+++ functions/test/story/data/story_logic.ts
@@ -1,20 +1,22 @@
 import { ClassicStoryLogic } from "../../../src/story";
 
 /**
- * A dummy story logic.
+ * A dummy story logic. Works with METADATA_0.
  */
 const duration = 1;
 const style = "style1";
-const characterName = "characterName1";
-const place = "place1";
-const characterFlaw = "characterFlaw1";
-const characterPower = "characterPower1";
-const characterChallenge = "characterChallenge1";
+const characterName = "frosty";
+const place = undefined;
+const object = undefined;
+const characterFlaw = "failure";
+const characterPower = undefined;
+const characterChallenge = "animal";
 export const CLASSIC_LOGIC_0 = new ClassicStoryLogic(
   duration,
   style,
   characterName,
   place,
+  object,
   characterFlaw,
   characterPower,
   characterChallenge
