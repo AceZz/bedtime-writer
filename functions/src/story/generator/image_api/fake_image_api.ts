837abc83af63f95caac5a760bc02cae3d00a8c44
AceZz <tristan.stampfler@gmail.com>
2023-08-26 15:55:08 +0200

    add seed to FakeImageApi

--- functions/src/story/generator/image_api/fake_image_api.ts
+++ functions/src/story/generator/image_api/fake_image_api.ts
@@ -6,11 +6,23 @@ export const FAKE_IMAGE_BYTES_0 = readFileSync(
   "src/story/generator/image_api/fake_image_api_0.jpg"
 );
 
-/**
- * Implementation of the `ImageApi` for fake data.
- */
+export const FAKE_IMAGE_BYTES_1 = readFileSync(
+  "src/story/generator/image_api/fake_image_api_1.jpg"
+);
+
 export class FakeImageApi implements ImageApi {
+  private readonly seed: 0 | 1;
+
+  constructor(seed?: 0 | 1) {
+    this.seed = seed ?? 0;
+  }
+
   async getImage(): Promise<Buffer> {
-    return FAKE_IMAGE_BYTES_0;
+    switch (this.seed) {
+      case 0:
+        return FAKE_IMAGE_BYTES_0;
+      case 1:
+        return FAKE_IMAGE_BYTES_1;
+    }
   }
 }
