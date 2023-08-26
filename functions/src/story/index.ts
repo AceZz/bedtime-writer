27951a570f43c4f610d566e3a982022f77b7b41c
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-26 23:56:09 +0200

    backend: add parseStoryStatus

--- functions/src/story/index.ts
+++ functions/src/story/index.ts
@@ -3,5 +3,6 @@ export * from "./generator";
 export * from "./logic";
 export { StoryMetadata } from "./story_metadata";
 export { StoryPart } from "./story_part";
-export { StoryStatus } from "./story_status";
+export { StoryReader } from "./story_reader";
+export { StoryStatus, parseStoryStatus } from "./story_status";
 export { StoryWriter } from "./story_writer";
