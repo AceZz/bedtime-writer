1f4ec9934eb85300ff08c89649d026dad85c7dd4
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-26 23:56:37 +0200

    backend: add StoryReader and FirebaseStoryReader

--- functions/src/firebase/story/index.ts
+++ functions/src/firebase/story/index.ts
@@ -4,4 +4,5 @@ export {
   FirestoreStoryRealtime,
 } from "./firestore_stories";
 export * from "./form";
+export { FirebaseStoryReader } from "./firebase_story_reader";
 export { FirebaseStoryWriter } from "./firebase_story_writer";
