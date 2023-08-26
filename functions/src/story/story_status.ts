c966a2e3fc39f839a384e7b08f1b1c74277eef95
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-26 23:56:09 +0200

    backend: add parseStoryStatus

--- functions/src/story/story_status.ts
+++ functions/src/story/story_status.ts
@@ -16,3 +16,18 @@ export enum StoryStatus {
    */
   ERROR = "error",
 }
+
+export function parseStoryStatus(value: string): StoryStatus {
+  switch (value) {
+    case "pending":
+      return StoryStatus.PENDING;
+    case "generating":
+      return StoryStatus.GENERATING;
+    case "complete":
+      return StoryStatus.COMPLETE;
+    case "error":
+      return StoryStatus.ERROR;
+    default:
+      throw new Error(`parseStoryStatus: unrecognized value ${value}`);
+  }
+}
