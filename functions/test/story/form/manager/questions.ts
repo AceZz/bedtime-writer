0fb4559cfa34c2dc5da5eaa7af51f0403b9b5305
Pierre Wan-Fat <p+github@wanfat.fr>
2023-08-31 00:20:16 +0200

    backend: slight refactor of form tests data
    `QUESTIONS` can be a const.

--- functions/test/story/form/manager/questions.ts
+++ functions/test/story/form/manager/questions.ts
@@ -1,45 +1,43 @@
 // We create the test data here because it relies on specific assumptions which
+// must not be affected by other code.
 
 import { StoryQuestion } from "../../../../src/story";
 import { dummyStoryChoices } from "../../data";
 
-// must not be affected by other code.
-export async function QUESTIONS(): Promise<StoryQuestion[]> {
-  const q0v0 = new StoryQuestion(
-    "q0v0",
-    "param0",
-    "Question 0 V0",
-    0,
-    new Date(2020, 0, 1),
-    await dummyStoryChoices(["q0v0-c0", "q0v0-c1", "q0v0-c2", "q0v0-c3"])
-  );
+export const Q0V0 = new StoryQuestion(
+  "q0v0",
+  "param0",
+  "Question 0 V0",
+  0,
+  new Date(2020, 0, 1),
+  dummyStoryChoices(["q0v0-c0", "q0v0-c1", "q0v0-c2", "q0v0-c3"])
+);
 
-  const q0v1 = new StoryQuestion(
-    "q0v1",
-    "param0",
-    "Question 0 V1",
-    0,
-    new Date(2021, 0, 1), // V1 hopefully comes after V0.
-    await dummyStoryChoices(["q0v1-c0", "q0v1-c1", "q0v1-c2", "q0v1-c3"])
-  );
+export const Q0V1 = new StoryQuestion(
+  "q0v1",
+  "param0",
+  "Question 0 V1",
+  0,
+  new Date(2021, 0, 1), // V1 hopefully comes after V0.
+  dummyStoryChoices(["q0v1-c0", "q0v1-c1", "q0v1-c2", "q0v1-c3"])
+);
 
-  const q1v0 = new StoryQuestion(
-    "q1v0",
-    "param1",
-    "Question 1 V0",
-    1,
-    new Date(2022, 0, 1),
-    await dummyStoryChoices(["q1v0-c0", "q1v0-c1", "q1v0-c2", "q1v0-c3"])
-  );
+export const Q1V0 = new StoryQuestion(
+  "q1v0",
+  "param1",
+  "Question 1 V0",
+  1,
+  new Date(2022, 0, 1),
+  dummyStoryChoices(["q1v0-c0", "q1v0-c1", "q1v0-c2", "q1v0-c3"])
+);
 
-  const q2v0 = new StoryQuestion(
-    "q2v0",
-    "param2",
-    "Question 2 V0",
-    1,
-    new Date(2022, 0, 1),
-    await dummyStoryChoices(["q2v0-c0", "q2v0-c1", "q2v0-c2", "q2v0-c3"])
-  );
+export const Q2V0 = new StoryQuestion(
+  "q2v0",
+  "param2",
+  "Question 2 V0",
+  1,
+  new Date(2022, 0, 1),
+  dummyStoryChoices(["q2v0-c0", "q2v0-c1", "q2v0-c2", "q2v0-c3"])
+);
 
-  return [q0v0, q0v1, q1v0, q2v0];
-}
+export const QUESTIONS = [Q0V0, Q0V1, Q1V0, Q2V0];
--- functions/test/story/form/manager/questions_variations.test.ts
+++ functions/test/story/form/manager/questions_variations.test.ts
@@ -1,20 +1,10 @@
-import { test, expect, describe, beforeAll } from "@jest/globals";
-import { StoryQuestion } from "../../../../src/story";
-import { QUESTIONS } from "./questions";
+import { test, expect, describe } from "@jest/globals";
+import { Q0V1 } from "./questions";
 import { iterQuestionsVariations } from "../../../../src/story/form/manager/questions_variations";
 
 describe("choice_combinations", () => {
-  let allQuestions: StoryQuestion[];
-  let q0v1: StoryQuestion;
-
-  // Check we are running in emulator mode before initializing Firebase.
-  beforeAll(async () => {
-    allQuestions = await QUESTIONS();
-    q0v1 = allQuestions[1];
-  });
-
   test("iterQuestionsVariations", () => {
-    const combinations = Array.from(iterQuestionsVariations(q0v1, 2));
+    const combinations = Array.from(iterQuestionsVariations(Q0V1, 2));
     expect(combinations.length).toBe(6);
 
     // Check unicity.
