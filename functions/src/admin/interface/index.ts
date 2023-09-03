0908690223b5acee5825474d8ea09e6b741d8645
AceZz <tristan.stampfler@gmail.com>
2023-09-03 15:52:12 +0200

    add single image view

--- functions/src/admin/interface/index.ts
+++ functions/src/admin/interface/index.ts
@@ -23,9 +23,10 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/form", async (req, res) => {
+  // Get form from request
   const selectedFormId = req.query.formId as string;
+
   if (!selectedFormId) {
-    // Handle missing formId, redirect to index
     res.redirect("/");
     return;
   }
@@ -33,29 +34,58 @@ app.get("/form", async (req, res) => {
   const firestore = new FirestoreContext();
   const storyReader = new FirebaseStoryReader(firestore.storyCacheLanding);
 
-  const storyIds = await storyReader.getFormStoryIds(selectedFormId);
-  const numStories = storyIds.length; // Get the count of stories
+  const storyIds = (await storyReader.getFormStoryIds(selectedFormId)).sort();
+  const numStories = storyIds.length;
 
   const allImages: {
-    [key: string]: { id: string; data: string; status: string | undefined }[];
+    [imageId: string]: {
+      storyId: string;
+      data: string;
+      status: string | undefined;
+    };
   } = {};
+
+  let allImageIds: string[] = [];
 
   for (const storyId of storyIds) {
     const imageIds = await storyReader.getImageIds(storyId);
-    const images = await Promise.all(
+    allImageIds = allImageIds.concat(imageIds);
+    await Promise.all(
       imageIds.map(async (imageId) => {
         const data = await storyReader.getImage(storyId, imageId);
         const status = await storyReader.getImageStatus(storyId, imageId);
-        return { id: imageId, data: data.toString("base64"), status: status };
+        allImages[imageId] = {
+          storyId: storyId,
+          data: data.toString("base64"),
+          status: status,
+        };
       })
     );
-    allImages[storyId] = images;
   }
+  allImageIds.sort();
 
+  // Get current imageId from request
+  if (req.query.imageId === undefined || req.query.imageId === null) {
+    req.query.imageId = allImageIds[0];
+  }
+  const currentImageId = req.query.imageId as string;
+
+  const currentImage = allImages[currentImageId];
+  const currentIndex = allImageIds.indexOf(currentImageId);
+  const nextIndex = (currentIndex + 1) % allImageIds.length;
+  const nextImageId = allImageIds[nextIndex];
+
+  const currentUiIndex = currentIndex + 1;
+  const maxUiIndex = allImageIds.length;
   res.render("form", {
     title: `Images from Form ID: ${selectedFormId}`,
-    allImages,
-    numStories: numStories,
+    currentImageId,
+    currentImage,
+    currentUiIndex,
+    maxUiIndex,
+    nextImageId,
+    selectedFormId,
+    numStories,
   });
 });
 
