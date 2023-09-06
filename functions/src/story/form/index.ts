bfc5f82456e4b4b076a57f42a91719c5e648029d
Pierre Wan-Fat <p+github@wanfat.fr>
2023-09-07 00:49:54 +0200

    backend: extract storyFormToFirestore
    This function is useful outside of `FirebaseStoryFormWriter`.

--- functions/src/story/form/index.ts
+++ functions/src/story/form/index.ts
@@ -1,8 +1,7 @@
 export { StoryChoice } from "./story_choice";
 export { StoryFormManager } from "./manager/story_form_manager";
-export { StoryForm } from "./story_form";
-export { StoryFormAnswerError } from "./story_form";
 export { StoryFormReader, StoryFormReaderParams } from "./story_form_reader";
+export { StoryForm, StoryFormAnswerError } from "./story_form";
 export { StoryFormWriter } from "./story_form_writer";
 export { StoryQuestion } from "./story_question";
 export {
