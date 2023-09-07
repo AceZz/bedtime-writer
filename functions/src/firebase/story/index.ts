13f7a30705e50595dfb8732b011de7a05546fcc7
Pierre Wan-Fat <p+github@wanfat.fr>
2023-09-07 20:24:29 +0200

    backend: add FirebaseStoryCopier

--- functions/src/firebase/story/index.ts
+++ functions/src/firebase/story/index.ts
@@ -4,5 +4,6 @@ export {
   FirestoreStoryRealtime,
 } from "./firestore_stories";
 export * from "./form";
+export { FirebaseStoryCopier } from "./firebase_story_copier";
 export { FirebaseStoryReader } from "./firebase_story_reader";
 export { FirebaseStoryWriter } from "./firebase_story_writer";
