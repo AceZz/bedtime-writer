7e7a5c761b57d12e248fb363ddeb7b9bf889ccc8
Pierre Wan-Fat <p+github@wanfat.fr>
2023-09-10 19:53:49 +0200

    refactor(backend): remove FirestoreStories.copyStory
    This method was replaced.

--- functions/src/firebase/story/firestore_stories.ts
+++ functions/src/firebase/story/firestore_stories.ts
@@ -63,44 +63,6 @@ export abstract class FirestoreStories extends FirestoreCollection {
   promptsRef(id: string): CollectionReference {
     return this.storyRef(id).collection("prompts");
   }
-
-  /**
-   * Copy a story to another FirestoreStories collection.
-   *
-   * The copy includes the story doc and its subcollections. It will
-   * fail in case the destination already has a story with the same id.
-   */
-  async copyStory(id: string, destination: FirestoreStories): Promise<void> {
-    const sourceStoryRef = this.storyRef(id);
-    // Fetch the story data
-    const storyData = (await sourceStoryRef.get()).data();
-    if (!storyData) {
-      throw new Error(`copyStory: story with id ${id} not found.`);
-    }
-
-    // Create or update the story in the destination collection
-    const destinationStoryRef = destination.storyRef(id);
-    await destinationStoryRef.create(storyData);
-
-    // Function to copy a subcollection
-    const copySubcollection = async (
-      sourceRef: CollectionReference,
-      destinationRef: CollectionReference
-    ) => {
-      const snapshot = await sourceRef.get();
-      const batch = destination.firestore.batch();
-      snapshot.docs.forEach((doc) => {
-        const destDocRef = destinationRef.doc(doc.id);
-        batch.set(destDocRef, doc.data());
-      });
-      return batch.commit();
-    };
-
-    // Copy subcollections
-    await copySubcollection(this.imagesRef(id), destination.imagesRef(id));
-    await copySubcollection(this.partsRef(id), destination.partsRef(id));
-    await copySubcollection(this.promptsRef(id), destination.promptsRef(id));
-  }
 }
 
 /**
