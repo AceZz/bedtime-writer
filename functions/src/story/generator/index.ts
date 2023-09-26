469b7f6eb0d920b49fbafa279e408402cf805070
AceZz <tristan.stampfler@gmail.com>
2023-09-26 20:39:47 +0200

    remove OnePartStoryGenerator

--- functions/src/story/generator/index.ts
+++ functions/src/story/generator/index.ts
@@ -1,6 +1,5 @@
 export { ImageApi, ImageApiConfig, IMAGE_SIZE_DEFAULT } from "./image_api";
 export { NPartStoryGenerator } from "./n_part_story_generator";
-export { OnePartStoryGenerator } from "./one_part_story_generator";
 export { StoryGenerator } from "./story_generator";
 export {
   TextApi,
