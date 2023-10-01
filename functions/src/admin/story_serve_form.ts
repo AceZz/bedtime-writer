7dbfc07c3442ff678148bed7de54e8d8c0b5c2dc
Pierre Wan-Fat <p+github@wanfat.fr>
2023-10-01 20:42:09 +0200

    refactor(backend): rename story_gen_forms
    Rename to `story_forms_gen`.

--- functions/src/admin/story_serve_form.ts
+++ functions/src/admin/story_serve_form.ts
@@ -128,7 +128,7 @@ async function promptFormId(
   if (unservedFormIds.length === 0) {
     console.log(
       "\nAll approved forms are already served. You may want to:\n" +
-        "  * Generate new forms with `npm run story_gen_forms`.\n" +
+        "  * Generate new forms with `npm run story_forms_gen`.\n" +
         "  * Cache generated forms with `npm run story_forms_cache`.\n" +
         "  * Approve cached forms with `npm run admin_interface`."
     );
